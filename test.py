ce457600d4e4f3423a7a2663b51c0bd6a6abef68
Anuruddha Perera <anuruddhaperera95@gmail.com>
2018-08-30 19:44:34 +0530

    python unit testing

--- test.py
+++ test.py
@@ -36,6 +36,39 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
 
+    def face_recognize(self):
+        tester = app.test_client(self)
+        response = tester.get('/recognize/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def authenticateUser(self):
+        tester = app.test_client(self)
+        response = tester.get('/authenticateuser/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+
+    def getfilldetail(self):
+        tester = app.test_client(self)
+        response = tester.get('/getfilldetail/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+
+    def filldetail(self):
+        tester = app.test_client(self)
+        response = tester.get('/filldetail/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def saveDetail(self):
+        tester = app.test_client(self)
+        response = tester.get('/savedetail/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def getProgress(self):
+        tester = app.test_client(self)
+        response = tester.get('/getProgress/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+
 if __name__ == '__main__':
     unittest.main()
 
