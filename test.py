1307d3e8067bbd88264a8324462b33d23905c5dc
Anuruddha Perera <anuruddhaperera95@gmail.com>
2018-08-30 13:47:46 +0530

    Testing routes

--- test.py
+++ test.py
@@ -14,6 +14,26 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/dash/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
+    def member_checkin(self):
+        tester = app.test_client(self)
+        response = tester.get('/checkin/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def settings(self):
+        tester = app.test_client(self)
+        response = tester.get('/settings/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+
+    def train_model(self):
+        tester = app.test_client(self)
+        response = tester.get('/train/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def initialize_model(self):
+        tester = app.test_client(self)
+        response = tester.get('/init/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
